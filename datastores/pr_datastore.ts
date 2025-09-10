6df9055246722f91b3f1a8f217baf199e5df6927
Francis Breidenbach <8886566+cheefbird@users.noreply.github.com>
2025-09-09 18:43:54 -0700

    refactor datastore for useable schema

--- datastores/pr_datastore.ts
+++ datastores/pr_datastore.ts
@@ -1,29 +1,28 @@
 import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
 
-const PullRequestDatastore = DefineDatastore({
+export const PullRequestDatastoreSchema = {
+  unique_id: {
+    type: Schema.types.string,
+  },
+  pr_url: {
+    type: Schema.types.string,
+  },
+  author: {
+    type: Schema.slack.types.user_id,
+  },
+  repo_name: {
+    type: Schema.types.string,
+  },
+  pr_number: {
+    type: Schema.types.string,
+  },
+  status: { // open, merged, closed, draft
+    type: Schema.types.string,
+  },
+};
+
+export default DefineDatastore({
   name: "PullRequests",
   primary_key: "unique_id",
-  attributes: {
-    // unique_id, repo, pr_number, author, status, url
-    unique_id: {
-      type: Schema.types.string,
-    },
-    pr_url: {
-      type: Schema.types.string,
-    },
-    author: {
-      type: Schema.slack.types.user_id,
-    },
-    repo_name: {
-      type: Schema.types.string,
-    },
-    pr_number: {
-      type: Schema.types.string,
-    },
-    status: { // open, merged, closed, draft
-      type: Schema.types.string,
-    },
-  },
+  attributes: PullRequestDatastoreSchema,
 });
-
-export default PullRequestDatastore;
